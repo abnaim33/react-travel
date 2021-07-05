84a3962efe70e1e1e183f2b1f3ea785df1fb67fa
abnaim33 <67532457+abnaim33@users.noreply.github.com>
2021-07-05 23:12:42 +0600

    third commit

--- README.md
+++
@@ -1,4 +0,0 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-# react-travel
-
-my linked-in profile=https://www.linkedin.com/in/md-naim-hossen-567679201/
--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -2,6 +2,5 @@ import React from 'react';
 import '../../App.css';
 
 export default function SignUp() {
-  return
-  <h1 className='sign-up'>Working on this page</h1>;
+  return <h1 className='sign-up'>Working on this page</h1>;
 }
